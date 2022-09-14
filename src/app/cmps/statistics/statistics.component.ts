08b2ad09b17f99f174a76f4d5ba5a7030ea5461b
Efart Rotem <ma.efrat@gmail.com>
2022-09-14 23:46:59 +0300

    Improve chart data and styling it

--- src/app/cmps/statistics/statistics.component.ts
+++ src/app/cmps/statistics/statistics.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core'
 import { Chart, registerables } from 'chart.js'
+import { Observable } from 'rxjs'
+import { BitcoinService } from 'src/app/services/bitcoin-service/bitcoin.service'
 
 @Component({
   selector: 'statistics',
@@ -8,39 +10,41 @@ import { Chart, registerables } from 'chart.js'
 })
 export class StatisticsComponent implements OnInit {
 
-  constructor() {
+  marketPrice!: Array<any>
+
+  constructor(private bitcoinService: BitcoinService) {
     Chart.register(...registerables)
-   }
+  }
 
   ngOnInit(): void {
-    console.log('statistics cmps')
+    this.marketPrice = this.bitcoinService.getMarketPrice()
+    console.log(this.marketPrice);
+
+
+    this.getMarketPrice()
+  }
+
+  getMarketPrice() {
+
     const canvas = document.getElementById('myChart') as HTMLCanvasElement
     const ctx = canvas?.getContext('2d')
-    
+
     const myChart = new Chart(ctx!, {
-      type: 'bar',
+      type: 'line',
       data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        // labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: this.marketPrice.map(item => item.x),
         datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
+          label: 'The average USD market price across major bitcoin exchanges.',
+          // data: [12, 19, 3, 5, 2, 3],
+          data: this.marketPrice.map(item => item.y),
           backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
+            'rgb(254, 202, 30)',
           ],
           borderColor: [
             'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
           ],
-          borderWidth: 1
+          borderWidth: 0.1
         }]
       },
       options: {
@@ -50,7 +54,7 @@ export class StatisticsComponent implements OnInit {
           }
         }
       }
-    });
+    })
   }
 
 }
